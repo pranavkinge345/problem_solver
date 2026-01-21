52df6c5f9fc7116183c75cb95cd2e3c76f5dc3aa
unknown <pranavkinge123@gmail.com>
2026-01-21 09:36:58 +0530

    Add brute force solution for Two Sum

--- main.cpp
+++ main.cpp
@@ -15,3 +15,17 @@ vector<int> twoSum(vector<int>& nums, int target) {
 
 // Time Complexity: O(n^2)
 
+#include <bits/stdc++.h>
+using namespace std;
+
+// Brute Force Approach
+vector<int> twoSum(vector<int>& nums, int target) {
+    for(int i = 0; i < nums.size(); i++) {
+        for(int j = i + 1; j < nums.size(); j++) {
+            if(nums[i] + nums[j] == target) {
+                return {i, j};
+            }
+        }
+    }
+    return {};
+}
